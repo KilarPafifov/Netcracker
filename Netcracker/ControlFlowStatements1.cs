15d61f2fa12d6d45cdca8469743bf8da735ab0a4
Kilar Pafifov <apafifova@list.ru>
2018-04-18 17:50:05 +0300

    add Get Function Value func

---
+++ Netcracker/ControlFlowStatements1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Netcracker
+{
+    class ControlFlowStatements1 : IControlFlowStatements1
+    {
+        public float GetFunctionValue(float x)
+        {
+            if(x > 0)
+            {
+                return 2 * (Math.Abs(x));
+            }
+            else
+            { 
+                return 6 - x;
+            }
+        }
+    }
+}
---
+++ Netcracker/ControlFlowStatements1Test.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Netcracker
+{
+    class ControlFlowStatements1Test 
+    {
+        public static bool TestGetFunctionValue()
+        {
+            ControlFlowStatements1 test = new ControlFlowStatements1();
+            if ((test.GetFunctionValue(-1)) == 7)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            
+            Console.ReadLine();
+        }
+    }
+}
